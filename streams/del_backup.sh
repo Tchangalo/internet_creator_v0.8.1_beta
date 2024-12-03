03b7d39ecb676ee1691f29c1975a9737134143b1
Tchangalo <153798615+Tchangalo@users.noreply.github.com>
2024-12-03 10:07:21 +0100

    Update del_backup.sh

--- streams/del_backup.sh
+++ streams/del_backup.sh
@@ -14,28 +14,24 @@ if [ -z "$provider" ] || [ -z "$fr" ] || [ -z "$lr" ]; then
     exit 1
 fi
 
-if [[ $(df -T / | awk 'NR==2 {print $2}') == "zfs" ]]
-	then
-		echo -e "${C}Deleting ALL existing backups${NC}"
-		sudo rm -rf /var/lib/vz/dump
-		sudo mkdir /var/lib/vz/dump
-        for i in $(seq $fr $lr)
-    	do 
-			vmid=${provider}0${provider}0$(printf '%02d' $i)
-			echo -e "${C}Backing up router ${vmid}${NC}"
-    		sudo vzdump ${provider}0${provider}00$i --dumpdir /var/lib/vz/dump --mode snapshot --compress 0
-            sleep 5
+echo -e "${C}Deleting ALL existing backups${NC}"
+if [[ $(df -T / | awk 'NR==2 {print $2}') == "zfs" ]]; then
+	sudo rm -rf /var/lib/vz/dump
+	sudo mkdir /var/lib/vz/dump
+        for i in $(seq $first_router $last_router); do 
+		vmid=${provider}0${provider}0$(printf '%02d' $i)
+		echo -e "${C}Backing up router ${vmid}${NC}"
+		sudo vzdump ${provider}0${provider}00$i --dumpdir /var/lib/vz/dump --mode snapshot --compress 0
+		sleep 5
     	done
 	else
-		echo -e "${C}Deleting ALL existing backups${NC}"
-        sudo rm -rf /var/lib/pve/local-btrfs/dump
-		sudo mkdir /var/lib/pve/local-btrfs/dump
-        for i in $(seq $fr $lr)
-    	do 
-			vmid=${provider}0${provider}0$(printf '%02d' $i)
-			echo -e "${C}Backing up VM ${vmid}${NC}"
-    		sudo vzdump ${provider}0${provider}00$i --dumpdir /var/lib/pve/local-btrfs/dump --mode snapshot --compress 0
-            sleep 5
+	sudo rm -rf /var/lib/pve/local-btrfs/dump
+	sudo mkdir /var/lib/pve/local-btrfs/dump
+	for i in $(seq $first_router $last_router); do 
+		vmid=${provider}0${provider}0$(printf '%02d' $i)
+		echo -e "${C}Backing up router ${vmid}${NC}"
+		sudo vzdump ${provider}0${provider}00$i --dumpdir /var/lib/pve/local-btrfs/dump --mode snapshot --compress 0
+		sleep 5
     	done
 fi  
 
